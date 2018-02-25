e66602a01a993f54e9eb6125f369af257685d752
viktharien <viktharien@zoho.com>
2018-02-25 11:28:35 -0600

    implement model validations

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,2 +1,5 @@
 class Word < ApplicationRecord
+    has_many :sentences, dependent: :destroy
+    validates :english, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 0, maximum: 25 }
+    validates :single, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 0, maximum: 40 }
 end
