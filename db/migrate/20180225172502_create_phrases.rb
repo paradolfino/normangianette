b5a417fc402b244c1f005a2a07c11214386f0b13
viktharien <viktharien@zoho.com>
2018-02-25 11:25:59 -0600

    generate migrations for words and phrases

--- db/migrate/20180225172502_create_phrases.rb
+++ db/migrate/20180225172502_create_phrases.rb
@@ -1,7 +1,9 @@
 class CreatePhrases < ActiveRecord::Migration[5.1]
   def change
     create_table :phrases do |t|
-
+      t.text :english
+      t.text :normansk
+      t.references :word, foreign_key: true
       t.timestamps
     end
   end
