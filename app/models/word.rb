d265e09cd2ed73f24a561fac0e869c8b04e1f098
viktharien <viktharien@zoho.com>
2018-02-25 14:19:48 -0600

    create word json views

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,5 +1,5 @@
 class Word < ApplicationRecord
-    has_many :sentences, dependent: :destroy
+    has_many :phrases, dependent: :destroy
     validates :eng_si, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 0, maximum: 25 }
     validates :nor_si, presence: true, uniqueness: { case_sensitive: false }, length: { minimum: 0, maximum: 40 }
 end
