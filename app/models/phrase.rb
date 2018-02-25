743fdba9c4bae5a00db8f229b07811f327c8c176
viktharien <viktharien@zoho.com>
2018-02-25 11:28:18 -0600

    implement model validations

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,2 +1,10 @@
 class Phrase < ApplicationRecord
+    belongs_to :word
+
+    validates :english,
+    presence: true,
+    length: { minimum: 3, maximum: 150 }
+    validates :normansk,
+    presence: true,
+    length: { minimum: 3, maximum: 150 }
 end
