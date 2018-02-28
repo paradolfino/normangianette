7ca9ec7a86a5bb35ca4a2f904e5dc6a5ad86c5a4
viktharien <viktharien@zoho.com>
2018-02-27 19:12:10 -0600

    remove unnecessary routes for html views such as new and edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :words do
+  resources :words, :only => [:index, :show] do
     resources :phrases
   end
 end
