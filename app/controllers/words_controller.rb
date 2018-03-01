919e329fd108e370c68c083842a8b678f2da4c9b
viktharien <viktharien@zoho.com>
2018-02-28 19:48:40 -0600

    testing json requests, deactivate cors protection for test

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -58,7 +58,7 @@ end
   private
 
   def word_params
-    params.permit(:eng_si, :nor_si, :nor_pl, :nor_def, :nor_defpl, :nor_past, :nor_pres, :nor_fut, :created_by)
+    params.permit(:eng_si, :nor_si, :nor_pl, :nor_def, :nor_defpl, :nor_past, :nor_pres, :nor_fut, :created_by, :category_id)
   end
 
   def set_word
