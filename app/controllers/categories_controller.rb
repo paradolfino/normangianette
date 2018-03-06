e7453e9693842735421b8a486f85bc8d936c824b
viktharien <viktharien@zoho.com>
2018-03-05 19:33:25 -0600

    chore: implement REST actions for categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,7 +18,7 @@ class CategoriesController < ApplicationController
       @category= Category.new(cat_params)
   
       if @category.save
-          format.json { json_response(@category) }
+          format.json { json_response(@categories.to_json(:include => [:words, :phrases])) }
       else
           format.json { json_response(@category.errors, :unprocessable_entity) }
       end
@@ -26,7 +26,10 @@ class CategoriesController < ApplicationController
     end
   
     def show
-      json_response(@category.to_json(:include => :word))
+      respond_to do |format|
+        format.html { render :show}
+        format.json { json_response(@categories.to_json(:include => [:words, :phrases])) }
+      end
     end
   
     def update
